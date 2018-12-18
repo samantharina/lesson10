f176ea4987b6cbc4baa94faf1a1d78d051c02408
samantharina <43784570+samantharina@users.noreply.github.com>
2018-12-18 09:55:00 -0500

    Add files via upload

---
+++ Lesson 10/Problem1/problem1.py
@@ -0,0 +1,12 @@
+print('-'*65)
+print('Life Decision Bot: ')
+print()
+rain = input('Is it raining outside?: ')
+if rain == 'yes':
+	print('Stay at home!')
+	print('Maybe binge watch a new show on Netflix?')
+else: 
+	print('Go out and do something!')
+	print('Maybe go on a hike!')
+
+print('-'*65)
---
+++ Lesson 10/Problem2/problem2.py
@@ -0,0 +1,10 @@
+print('-'*65)
+print('Best Borough Bot: ')
+print()
+place = input('What borough are you from?: ')
+if place == 'Brooklyn':
+	print('Nice, Brooklyn is the best borough!')
+else:
+	print('Too bad, Brookyn is the best borough.')
+
+print('-'*65)
